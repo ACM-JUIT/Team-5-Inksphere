bd1a18862283285fcfa250f3c2d12103b20506da
Aditya-Kumar13 <aditya.kumar140706@gmail.com>
2026-06-08 08:00:37 +0530

    Logout Feature Added

--- backend/src/Controllers/authController.js
+++ backend/src/Controllers/authController.js
@@ -52,8 +52,14 @@ const login = async (req,res)=>{
         message:"Login successful"
     })
 }
+const logout = async (req,res)=>{
+    res.clearCookie('token');
+    return res.status(200).json({
+        message:"Logout successful"
+    })
+}
 
 
 module.exports = {
-    register,login
+    register,login,logout
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,6 +4,7 @@ const authController = require('../Controllers/authController');
 
 router.post('/register',authController.register);
 router.post('/login',authController.login);
+router.post('/logout',authController.logout);
 
 
 module.exports = router;
