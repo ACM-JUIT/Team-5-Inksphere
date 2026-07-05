230d0c0516f8f97d7d3902a87021c4f786b3710a
Aditya-Kumar13 <aditya.kumar140706@gmail.com>
2026-07-05 09:48:28 +0530

    changed some values

--- backend/src/Controllers/authController.js
+++ backend/src/Controllers/authController.js
@@ -31,7 +31,7 @@ const register = async (req,res)=>{
     res.cookie('token',token,{
         httpOnly : true,
         secure:true,
-        sameSite: "strict"
+        sameSite: "none"
     });
     
     res.status(201).json({
@@ -64,7 +64,7 @@ const login = async (req,res)=>{
     res.cookie('token',token,{
         httpOnly:true,
         secure:true,
-        sameSite:true
+        sameSite:none
     });
     return res.status(200).json({
         message:"Login successful"
